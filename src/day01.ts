9ef9e7ce89a37428fd97d31a3e1f875cb554a3f1
Tom Sherman <the.tomsherman@gmail.com>
2023-12-03 10:32:24 +0000

    Stupid trick edge case...

--- src/day01.ts
+++ src/day01.ts
@@ -22,24 +22,31 @@ export function solvePart2(input: string) {
 }
 
 const DIGIT_LOOKUP = {
-  "one": "1",
-  "two": "2",
-  "three": "3",
-  "four": "4",
-  "five": "5",
-  "six": "6",
-  "seven": "7",
-  "eight": "8",
-  "nine": "9",
-  "1": "1",
-  "2": "2",
-  "3": "3",
-  "4": "4",
-  "5": "5",
-  "6": "6",
-  "7": "7",
-  "8": "8",
-  "9": "9",
+  "oneight": ["1", "8"],
+  "twone": ["2", "1"],
+  "threeight": ["3", "8"],
+  "fiveight": ["5", "8"],
+  "sevenine": ["7", "9"],
+  "eightwo": ["8", "2"],
+  "nineight": ["9", "8"],
+  "one": ["1"],
+  "two": ["2"],
+  "three": ["3"],
+  "four": ["4"],
+  "five": ["5"],
+  "six": ["6"],
+  "seven": ["7"],
+  "eight": ["8"],
+  "nine": ["9"],
+  "1": ["1"],
+  "2": ["2"],
+  "3": ["3"],
+  "4": ["4"],
+  "5": ["5"],
+  "6": ["6"],
+  "7": ["7"],
+  "8": ["8"],
+  "9": ["9"],
 };
 
 const DIGITS = Object.keys(DIGIT_LOOKUP) as (keyof typeof DIGIT_LOOKUP)[];
@@ -54,7 +61,7 @@ function getNumbers(line: string) {
     }
 
     const digit = DIGIT_LOOKUP[digitString];
-    numbers.push(digit);
+    numbers.push(...digit);
     line = line.slice(digitString.length);
   }
 
