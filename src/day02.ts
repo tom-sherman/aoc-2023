a1bf97966aa93450b65283280c67424511a2b73f
Tom Sherman <the.tomsherman@gmail.com>
2023-12-04 23:30:25 +0000

    Part 2

--- src/day02.ts
+++ src/day02.ts
@@ -1,27 +1,46 @@
 import { sum } from "lib/arr-util.ts";
 
-type Grab = { blue: number; red: number; green: number };
+type Grab = { blue?: number; red?: number; green?: number };
 type Game = {
   id: number;
   grabs: Grab[];
 };
 
-const MAX_GRAB: Grab = {
-  red: 12,
-  green: 13,
-  blue: 14,
-};
-
 export function solvePart1(input: string) {
+  const MAX_GRAB = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
   const games = input.split("\n").map(parseGame);
   const validGames = games.filter((game) => {
-    return game.grabs.every((grab) =>
-      grab.blue <= MAX_GRAB.blue && grab.red <= MAX_GRAB.red &&
-      grab.green <= MAX_GRAB.green
+    return game.grabs.every(({ blue = 0, red = 0, green = 0 }) =>
+      blue <= MAX_GRAB.blue && red <= MAX_GRAB.red && green <= MAX_GRAB.green
     );
   });
 
   return sum(validGames.map((game) => game.id));
+}
+
+export function solvePart2(input: string) {
+  const games = input.split("\n").map(parseGame);
+  const powers = games.map((game) => {
+    const minGrab = {
+      blue: -Infinity,
+      red: -Infinity,
+      green: -Infinity,
+    };
+
+    for (const { blue, red, green } of game.grabs) {
+      if (blue) minGrab.blue = Math.max(minGrab.blue, blue);
+      if (red) minGrab.red = Math.max(minGrab.red, red);
+      if (green) minGrab.green = Math.max(minGrab.green, green);
+    }
+
+    return minGrab.blue * minGrab.red * minGrab.green;
+  });
+
+  return sum(powers);
 }
 
 function parseGame(input: string): Game {
@@ -31,7 +50,7 @@ function parseGame(input: string): Game {
   }
   const id = Number(idPart.slice("Game ".length));
   const grabs = grabsPart.split("; ").map((grabString) => {
-    const grab: Grab = { blue: 0, red: 0, green: 0 };
+    const grab: Grab = {};
     for (const s of grabString.split(", ")) {
       const [n, c] = s.split(" ");
       if (c !== "blue" && c !== "red" && c !== "green") {
